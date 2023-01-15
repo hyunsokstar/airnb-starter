daaf7d402c02a6e7385fa8aa6aa4103417e8773f
hyunsokstar <hyunsokstar@gmail.com>
2023-01-15 22:17:45 +0900

    update for 0115

--- houses/admin.py
+++ houses/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import House
 
-admin.site.register(House)
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display=["name", "price", "address", "description"]
+    list_filter = ["pets_allowed"]
--- houses/models.py
+++ houses/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 
 class House(models.Model):
-
     """ HOUSE MODEL """
-
     name = models.CharField(max_length=140)
     price = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.name    
