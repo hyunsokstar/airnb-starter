6368ecc718f2a209adcff426cf9c6db35f96dde8
hyunsokstar <hyunsokstar@gmail.com>
2023-01-15 22:38:32 +0900

    update for drf-starter-level1 ca15

--- houses/admin.py
+++ houses/admin.py
@@ -4,5 +4,6 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    list_display=["name", "price", "address", "description"]
+    list_display = ["name", "price", "address"]
     list_filter = ["pets_allowed"]
+    search_fields= ["address"]
